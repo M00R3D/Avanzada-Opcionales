4c18f49e0f9161a5a74e1442b82e856e41d107d1
M00R3D <jobmurdan@hotmail.com>
2024-11-20 02:50:37 -0700

    ahora puedo dibujar la linea de manera continua

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
-const myCanvas = document.getElementById("myCanvas");const ctx = myCanvas.getContext("2d");
+const myCanvas = document.getElementById("myCanvas");
+const ctx = myCanvas.getContext("2d");
+let drawing = false;let var_x = 0;let var_y = 0;
 
-myCanvas.addEventListener('mousedown', (event) => {
-    const rect = myCanvas.getBoundingClientRect();
-    const x = event.clientX - rect.left;
-    const y = event.clientY - rect.top;
-    dibujar(x, y);
+ctx.strokeStyle = "blue";
+ctx.lineWidth = 5; 
+ctx.lineCap = "round";
+
+myCanvas.addEventListener("mousedown", (e) => {
+    drawing = true; 
+        var_x = e.clientX - myCanvas.getBoundingClientRect().left;
+        var_y = e.clientY - myCanvas.getBoundingClientRect().top;
 });
 
-function dibujar(x, y) 
-{
-    ctx.fillStyle = "black"; 
-    ctx.fillRect(x, y, 3, 3);
-}
+myCanvas.addEventListener("mousemove", (e) => {
+    if (!drawing) return; 
+    const x = e.clientX - myCanvas.getBoundingClientRect().left;
+    const y = e.clientY - myCanvas.getBoundingClientRect().top;
+        ctx.beginPath();
+        ctx.moveTo(var_x, var_y);  ctx.lineTo(x, y);
+        ctx.stroke();
+    var_x = x; var_y = y;
+});
+myCanvas.addEventListener("mouseup", () => {drawing = false;});
+myCanvas.addEventListener("mouseleave", () => {drawing = false;});
