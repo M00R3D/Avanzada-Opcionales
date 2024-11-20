19104027384ba3e954393803115be97c2a1ff7bf
M00R3D <jobmurdan@hotmail.com>
2024-11-20 02:21:04 -0700

    ahora puedo dibujar puntos dando click

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-const myCanvas = document.getElementById("myCanvas");
-const ctx = myCanvas.getContext("2d");
-ctx.fillStyle = "red";
-ctx.fillRect(20, 20, 150, 100);
+const myCanvas = document.getElementById("myCanvas");const ctx = myCanvas.getContext("2d");
+
+myCanvas.addEventListener('mousedown', (event) => {
+    const rect = myCanvas.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+    dibujar(x, y);
+});
+
+function dibujar(x, y) 
+{
+    ctx.fillStyle = "black"; 
+    ctx.fillRect(x, y, 3, 3);
+}
