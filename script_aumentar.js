dbabe91d066f9e4db9b1015c23593b5b5c57a84f
M00R3D <jobmurdan@hotmail.com>
2024-11-27 00:41:29 -0700

    JS: Utilizando canvas y las funciones de javascript necesarias

--- script_aumentar.js
+++ script_aumentar.js
@@ -1,14 +1,15 @@
 const myCanvas = document.getElementById("myCanvas");
 const ctx = myCanvas.getContext("2d");
 let drawing = false;let var_x = 0;let var_y = 0;
+let width = 10;
+const maxWidth = 50;
 
 ctx.strokeStyle = "blue";
 ctx.shadowColor = "blue";
-ctx.lineWidth = 6; 
 ctx.lineCap = "round";
+
 myCanvas.addEventListener("mousedown", (e) => {
     drawing = true; 
-        ctx.shadowBlur=4;
         var_x = e.clientX - myCanvas.getBoundingClientRect().left;
         var_y = e.clientY - myCanvas.getBoundingClientRect().top;
 });
@@ -17,10 +18,17 @@ myCanvas.addEventListener("mousemove", (e) => {
     if (!drawing) return; 
     const x = e.clientX - myCanvas.getBoundingClientRect().left;
     const y = e.clientY - myCanvas.getBoundingClientRect().top;
-        ctx.beginPath();
-        ctx.moveTo(var_x, var_y);  ctx.lineTo(x, y);
-        ctx.stroke();
+
+    if (width < maxWidth) 
+    {
+        width += 0.5; 
+    }
+    ctx.lineWidth = width;
+    ctx.shadowBlur = width / 2; 
+    ctx.beginPath();
+    ctx.moveTo(var_x, var_y);  ctx.lineTo(x, y);
+    ctx.stroke();
     var_x = x; var_y = y;
 });
-myCanvas.addEventListener("mouseup", () => {drawing = false;});
-myCanvas.addEventListener("mouseleave", () => {drawing = false;});
+myCanvas.addEventListener("mouseup", () => {drawing = false;width = 10;});
+myCanvas.addEventListener("mouseleave", () => {drawing = false;width = 10;});
