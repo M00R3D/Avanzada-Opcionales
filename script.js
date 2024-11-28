8b01c0678bc057ae42e2ce8b10ca0e6efad76277
job moore <jobmurdan@ohtmail.com>
2024-11-28 16:51:11 -0700

    JS: Utilizando canvas y las funciones de javascript necesarias

--- script.js
+++ script.js
@@ -3,26 +3,22 @@ const ctx = myCanvas.getContext("2d");
 let drawing = false;
 let var_x = 0;
 let var_y = 0;
-ctx.strokeStyle = "purple";
+ctx.strokeStyle = "black";
 // ctx.lineWidth = 4; 
-ctx.lineCap = "round";
+ctx.lineCap = "square";
 // ctx.setLineDash([2, 19]);
-function drawScribbleLine(x1, y1, x2, y2) {
-    const lines = 3;
-    const maxOffset = 3;
+function drawLine(x1, y1, x2, y2) {
+    const maxOffset = 0;
+    const offsetX1 = x1 + ( maxOffset - maxOffset / 2);
+    const offsetY1 = y1 + ( maxOffset - maxOffset / 2);
+    const offsetX2 = x2 + ( maxOffset - maxOffset / 2);
+    const offsetY2 = y2 + ( maxOffset - maxOffset / 2);
 
-    for (let i = 0; i < lines; i++) {
-        const offsetX1 = x1 + (Math.random() * maxOffset - maxOffset / 2);
-        const offsetY1 = y1 + (Math.random() * maxOffset - maxOffset / 2);
-        const offsetX2 = x2 + (Math.random() * maxOffset - maxOffset / 2);
-        const offsetY2 = y2 + (Math.random() * maxOffset - maxOffset / 2);
-
-        ctx.beginPath();
-        ctx.moveTo(offsetX1, offsetY1);
-        ctx.lineTo(offsetX2, offsetY2);
-        ctx.lineWidth = Math.random() * 1 + 1;
-        ctx.stroke();
-    }
+    ctx.beginPath();
+    ctx.moveTo(offsetX1, offsetY1);
+    ctx.lineTo(offsetX2, offsetY2);
+    ctx.lineWidth =  8;
+    ctx.stroke();
 }
 
 myCanvas.addEventListener("mousedown", (e) => {
@@ -35,7 +31,7 @@ myCanvas.addEventListener("mousemove", (e) => {
     if (!drawing) return;
     const x = e.clientX - myCanvas.getBoundingClientRect().left;
     const y = e.clientY - myCanvas.getBoundingClientRect().top;
-    drawScribbleLine(var_x, var_y, x, y);
+    drawLine(var_x, var_y, x, y);
     var_x = x;
     var_y = y;
 });
