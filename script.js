9e035c5de05bc7d9f1a33e0e09c1b721957fb6e2
job moore <jobmurdan@ohtmail.com>
2024-11-28 17:18:51 -0700

    JS: Utilizando canvas y las funciones de javascript necesarias

--- script.js
+++ script.js
@@ -4,36 +4,30 @@ let drawing = false;
 let var_x = 0;
 let var_y = 0;
 
-function drawTattooLine(x1, y1, x2, y2) {
-    const lineCount = 5; 
-    const lineSpacing = 4; 
-
-    for (let i = 0; i < lineCount; i++) {
-        const offset = (i - Math.floor(lineCount / 2)) * lineSpacing;
-        ctx.beginPath();
-        ctx.moveTo(x1 + offset, y1);
-        ctx.lineTo(x2 + offset, y2);
-        ctx.lineWidth = 1;
-        ctx.strokeStyle = "black";
-        ctx.stroke();
-    }
+function drawCircle(x, y) {
+    const radius = 14; 
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, Math.PI * 2); 
+    ctx.fillStyle = "red";
+    ctx.fill();
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 1;
+    ctx.stroke();
 }
 
 myCanvas.addEventListener("mousedown", (e) => {
     drawing = true;
     var_x = e.clientX - myCanvas.getBoundingClientRect().left;
     var_y = e.clientY - myCanvas.getBoundingClientRect().top;
+    drawCircle(var_x, var_y);
 });
 
 myCanvas.addEventListener("mousemove", (e) => {
     if (!drawing) return;
     const x = e.clientX - myCanvas.getBoundingClientRect().left;
     const y = e.clientY - myCanvas.getBoundingClientRect().top;
-    drawTattooLine(var_x, var_y, x, y);
-    var_x = x;
-    var_y = y;
+    drawCircle(x, y);
 });
-
 
 myCanvas.addEventListener("mouseup", () => {drawing = false;});
 myCanvas.addEventListener("mouseleave", () => {drawing = false;});
