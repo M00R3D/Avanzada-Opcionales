68dc6e16412ee4776436f72927eb5af229db5a3e
job moore <jobmurdan@ohtmail.com>
2024-11-28 17:02:29 -0700

    JS: Utilizando canvas y las funciones de javascript necesarias

--- script.js
+++ script.js
@@ -3,22 +3,22 @@ const ctx = myCanvas.getContext("2d");
 let drawing = false;
 let var_x = 0;
 let var_y = 0;
-ctx.strokeStyle = "black";
-// ctx.lineWidth = 4; 
-ctx.lineCap = "square";
-// ctx.setLineDash([2, 19]);
-function drawLine(x1, y1, x2, y2) {
-    const maxOffset = 0;
-    const offsetX1 = x1 + ( maxOffset - maxOffset / 2);
-    const offsetY1 = y1 + ( maxOffset - maxOffset / 2);
-    const offsetX2 = x2 + ( maxOffset - maxOffset / 2);
-    const offsetY2 = y2 + ( maxOffset - maxOffset / 2);
 
-    ctx.beginPath();
-    ctx.moveTo(offsetX1, offsetY1);
-    ctx.lineTo(offsetX2, offsetY2);
-    ctx.lineWidth =  8;
-    ctx.stroke();
+function drawMarkerLine(x1, y1, x2, y2) {
+    const offset = 20; 
+
+    for (let i = 0; i <= offset; i++) {
+        const gradient = ctx.createLinearGradient(x1, y1, x2 + i, y2 + i);
+        gradient.addColorStop(0, "rgba(0, 0, 0, 1)");
+        gradient.addColorStop(1, "rgba(0, 0, 0, 0.3)");
+
+        ctx.strokeStyle = gradient;
+        ctx.beginPath();
+        ctx.moveTo(x1 + i, y1 + i);
+        ctx.lineTo(x2 + i, y2 + i);
+        ctx.lineWidth = 2;
+        ctx.stroke();
+    }
 }
 
 myCanvas.addEventListener("mousedown", (e) => {
@@ -31,9 +31,10 @@ myCanvas.addEventListener("mousemove", (e) => {
     if (!drawing) return;
     const x = e.clientX - myCanvas.getBoundingClientRect().left;
     const y = e.clientY - myCanvas.getBoundingClientRect().top;
-    drawLine(var_x, var_y, x, y);
+    drawMarkerLine(var_x, var_y, x, y);
     var_x = x;
     var_y = y;
 });
+
 myCanvas.addEventListener("mouseup", () => {drawing = false;});
 myCanvas.addEventListener("mouseleave", () => {drawing = false;});
